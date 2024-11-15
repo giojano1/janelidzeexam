2eae3df66ade4020c3453eb0066f8677bd382abd
giojano1 <giorgi.janelidze64@gmail.com>
2024-11-15 20:47:06 +0400

    done

--- app/page.tsx
+++ app/page.tsx
@@ -33,65 +33,69 @@ const quizQuestions: QuizQuestion[] = [
 ];
 
 const QuizPage = () => {
-  // შექმენით 3 ცვლადი და მიანიჭეთ მათ შესაბამისი საწყისი მნიშვნელობები
-
-  // პირველი ცვლადი : currentQuestion - მიმდინარე კითხვის ინდექსი (რომ გავიგოთ მერამდენე კითცვაზეა მომხმარებელი)
-
-  // მეორე ცლვადი: score - მომხმარებლის ქულა, სამომავლოდ დავუმატებთ ერთს თუ სწორი იქნება,
-
-  // მესამე ცვლადი : isCompleted - ქვიზის დასრულების სტატუსი (რომ გავიგოთ მართლა დასრულდა თუ არა ქვიზი)
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [isCompleted, setIsCompleted] = useState(false);
 
   const handleAnswer = (selectedOption: string): void => {
-    console.log(selectedOption);
-    // ამჟამინდელი კითხვის შენახვა
-
-    // შევამოწმოთ სწორი არის თუ არა კითხვა რომელიც მომხარებელმა აირჩია
-
-    // გადავიყვანოთ შემდეგ კითხვაზე და თუ კითხვა ბოლოა დავასრულოთ ქვიზი
+    const isRight =
+      selectedOption === quizQuestions[currentQuestion].correctAnswer;
+    if (isRight) {
+      setScore((prev) => prev + 1);
+    }
+    if (quizQuestions.length === currentQuestion + 1) {
+      setIsCompleted(true);
+    } else {
+      setCurrentQuestion((prev) => prev + 1);
+    }
   };
 
   // შექმენით ფუნქცია რომელიც განაახლებს ქვიზის სტატუსს და დასრულებს ქვიზს
-  const restartQuiz = (): void => {};
+  const restartQuiz = (): void => {
+    setScore(0);
+    setCurrentQuestion(0);
+    setIsCompleted(false);
+  };
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-6">
       <div className="bg-white rounded-lg shadow-lg p-8 max-w-md text-center">
-        {/* თუ შესრულებულია გამოჩდეს ეს  */}
-        <>
-          <h1 className="text-2xl font-bold mb-4">Quiz Completed!</h1>
-          <p className="text-lg mb-6">
-            Your Score:{" "}
-            <span className="font-semibold"> Dynamic Score Here</span> /{" "}
-            {quizQuestions.length}
-          </p>
-          <button
-            onClick={restartQuiz}
-            className="bg-blue-500 text-white px-6 py-3 rounded-lg shadow hover:bg-blue-600 transition"
-          >
-            Restart Quiz
-          </button>
-        </>
-        {/* თუ ქვიზი მიმდინარეობს გამოჩნდეს ეს */}
-        <>
-          <p className="text-gray-600 mb-4">
-            Question {"ცვლადი აქ"} of {quizQuestions.length}
-          </p>
-          <h1 className="text-2xl font-bold mb-4">
-            "ქვიზიდან კითხვა გამოჩნდეს აქ"
-          </h1>
-          <div className="space-y-4">
-            {/* აქ სტატიკურად  ინდექსად 0 ელემენტის 4 კითხვა იმაპება ჩვენ გვინდა ყოველ შემდეგ კითხვაზე ახლიდან დაიმაპოს*/}
-            {quizQuestions[0].options.map((option) => (
-              <button
-                key={option}
-                onClick={() => handleAnswer(option)}
-                className="block w-full bg-gray-200 text-gray-800 px-4 py-2 rounded-lg shadow hover:bg-gray-300 transition"
-              >
-                {option}
-              </button>
-            ))}
-          </div>
-        </>
+        {isCompleted ? (
+          <>
+            <h1 className="text-2xl font-bold mb-4">Quiz Completed!</h1>
+            <p className="text-lg mb-6">
+              Your Score:
+              <span className="font-semibold"> {score}</span> /
+              {quizQuestions.length}
+            </p>
+            <button
+              onClick={restartQuiz}
+              className="bg-blue-500 text-white px-6 py-3 rounded-lg shadow hover:bg-blue-600 transition"
+            >
+              Restart Quiz
+            </button>
+          </>
+        ) : (
+          <>
+            <p className="text-gray-600 mb-4">
+              Question {currentQuestion + 1} of {quizQuestions.length}
+            </p>
+            <h1 className="text-2xl font-bold mb-4">
+              {quizQuestions[currentQuestion].question}
+            </h1>
+            <div className="space-y-4">
+              {quizQuestions[currentQuestion].options.map((option) => (
+                <button
+                  key={option}
+                  onClick={() => handleAnswer(option)}
+                  className="block w-full bg-gray-200 text-gray-800 px-4 py-2 rounded-lg shadow hover:bg-gray-300 transition"
+                >
+                  {option}
+                </button>
+              ))}
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
